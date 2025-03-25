a4f2e6a35efc77fe67fa10944870fa0e9dc092c5
Adam Zalewski <zalewskiadam220@gmail.com>
2025-03-25 17:36:27 +0100

    testing wss

--- wssServer/src/index.ts
+++ wssServer/src/index.ts
@@ -7,10 +7,10 @@ console.log(wss)
 
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
+    ws.send('Websocket connection established');
 
     ws.on('message', function message(data) {
         console.log('received: %s', data);        
         ws.send(("data recieved " + data));
     });
-    ws.send('something');
 });
